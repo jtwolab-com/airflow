3b7911b94966c92448671b104a4183cb8b78c788
eric.yoo <yoosungung@gmail.com>
2022-08-12 11:06:38 +0900

    081211

--- batch_dags/test.py
+++ batch_dags/test.py
@@ -1,3 +1,4 @@
+"""
 import json
 import pendulum
 from airflow.decorators import dag, task
@@ -12,20 +13,9 @@ def tutorial_taskflow_api_etl():
     @task()
     def extract():
         data_string = '{"1001": 301.27, "1002": 433.21, "1003": 502.22}'
-
         order_data_dict = json.load(data_string)
         return order_data_dict
 
-
-    order_data = extract()
-#   order_summary = transform(order_data)
-#   load(order_summary["total_order_value"])
-
-tutorial_etl_dag = tutorial_taskflow_api_etl()
-
-
-
-"""
     @task(multiple_outputs=True)
     def transform(order_data_dict: dict):
         total_order_value = 0
@@ -38,6 +28,13 @@ tutorial_etl_dag = tutorial_taskflow_api_etl()
     @task()
     def load(total_order_value: float):
         print(f"Total order value is: {total_order_value:.2f}")
+
+    order_data = extract()
+    order_summary = transform(order_data)
+    load(order_summary["total_order_value"])
+
+tutorial_etl_dag = tutorial_taskflow_api_etl()
+"""
 
 import datetime
 
